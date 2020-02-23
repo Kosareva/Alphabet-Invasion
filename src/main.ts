70041ed237df4dcad7a5ac7f1028e2d86692d573
lizaveta <kosareva962@gmail.com>
2020-02-23 23:43:45 +0200

    buggy code :)

---
+++ src/main.ts
@@ -0,0 +1,88 @@
+import {BehaviorSubject, combineLatest, fromEvent, interval} from 'rxjs';
+import {map, scan, startWith, switchAll, switchMap, takeWhile} from 'rxjs/operators';
+import {Letters, State} from './interfaces';
+
+const randomLetter: () => string = () => String.fromCharCode(Math.random() * ('z'.charCodeAt(0) - 'a'.charCodeAt(0)) + 'a'.charCodeAt(0));
+const levelChangeThreshold = 2;
+const speedAdjust = 50;
+const endThreshold = 15;
+const gameWidth = 30;
+
+const intervalSubject = new BehaviorSubject(600);
+
+const letters$ = intervalSubject
+    .pipe(
+        switchMap(i =>
+            interval(i)
+                .pipe(
+                    scan<number, Letters>(
+                        (letters) => ({
+                            intrvl: i,
+                            ltrs: [
+                                {
+                                    letter: randomLetter(),
+                                    yPos: Math.floor(Math.random() * gameWidth)
+                                },
+                                ...letters.ltrs
+                            ]
+                        }),
+                        {
+                            ltrs: [],
+                            intrvl: 0
+                        }
+                    )
+                )
+        )
+    );
+
+const keys$ = fromEvent(document, 'keydown')
+    .pipe(
+        startWith({key: ''}),
+        map((e: KeyboardEvent) => e.key)
+    );
+
+const renderGame = (state: State) => (
+    (
+        document.body.innerHTML = `Score: ${state.score}, Level: ${state.level}, <br/>`
+    ),
+        state.letters.forEach(l =>
+            (document.body.innerHTML += (<any>'&nbsp').repeat(l.yPos) + l.letter + '<br/>')
+        ),
+        (
+            document.body.innerHTML += (<any>'<br/>').repeat(endThreshold - state.letters.length - 1) + (<any>'-').repeat(gameWidth)
+        )
+);
+
+const renderGameOver = () => (document.body.innerHTML += '<br/> GAME OVER');
+const noop = () => {
+};
+
+const game$ = combineLatest([keys$, letters$])
+    .pipe(
+        scan<[string, Letters], State>(
+            (state, [key, letters]) => (
+                letters.ltrs[letters.ltrs.length - 1] && letters.ltrs[letters.ltrs.length - 1].letter === key
+                    ? ((state.score += 1), letters.ltrs.pop())
+                    : noop,
+                    state.score > 0 && state.score % levelChangeThreshold === 0
+                        ? ((letters.ltrs = []),
+                            (state.level += 1),
+                            (state.score += 1),
+                            intervalSubject.next(letters.intrvl - speedAdjust))
+                        : noop,
+                    {
+                        score: state.score,
+                        letters: letters.ltrs,
+                        level: state.level
+                    }
+            ),
+            {
+                score: 0,
+                letters: [],
+                level: 1
+            }
+        ),
+        takeWhile(state => state.letters.length < endThreshold),
+    );
+
+game$.subscribe(renderGame, (err) => console.warn(err), renderGameOver);
