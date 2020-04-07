7f8f1144e4db7703d7c2d105f26970bb1c4f1b99
Yelyzaveta_Kosarieva <yelyzaveta.kosarieva@refinitiv.com>
2020-04-07 23:48:33 +0300

    render game over

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import {BehaviorSubject, combineLatest, fromEvent, interval} from 'rxjs';
+import { BehaviorSubject, combineLatest, fromEvent, interval } from 'rxjs';
 import {
     filter,
-    map, mapTo,
+    first,
+    map,
+    mapTo,
     scan,
     startWith,
     switchAll,
     switchMap,
+    takeWhile,
 } from 'rxjs/operators';
-import {Letters, State} from './interfaces';
+import { Letters, State } from './interfaces';
 
 const randomLetter: () => string = () => String.fromCharCode(Math.random() * ('z'.charCodeAt(0) - 'a'.charCodeAt(0)) + 'a'.charCodeAt(0));
 const levelChangeThreshold = 20;
@@ -89,11 +92,13 @@ const game$ = combineLatest([keys$, letters$])
                 level: 1
             }
         ),
+        takeWhile(state => state.letters.length < endThreshold)
     );
 
 const startGame$ = fromEvent(document, 'keydown')
     .pipe(
-        filter(e => e.keyCode === 13),
+        filter(e => (<any>e).keyCode === 13),
+        first(),
         mapTo(game$),
         switchAll()
     );
